31869cc3e60b93b687981dce0e4a3eec02909de0
Emma Poliakova <2389098p@student.gla.ac.uk>
2022-01-19 12:34:06 +0000

    new nes demo

--- src/controller_receive.js
+++ src/controller_receive.js
@@ -3,15 +3,21 @@ import "smartcontroller";
 //** A simple browser side demo showing how to create a smartcontroller Nes object and access fields from each connected controller. */
 
 //create an instance of NesSmartController, optionally specify a peer ID for the PC browser peer
-const simplePeer = new smartcontroller.NesSmartController("123456789");
+//first connected is true by default, onle the first player to connect to a specific player ID will be allowed to keep it, the rest of the connections to the same ID won't be allowed
+const simplePeer = new smartcontroller.NesSmartController(
+  "123456789",
+  (firstConnected = true)
+);
 
-//create and display a QR code for the smartphones, specify url for the controller, div element for the code to be displayed, optionally size and a player ID
-//this url is an official Nes Controller compatible with the NesSmartController class
+//create and display a QR code for the smartphones, specify url for the controller, div element for the code to be displayed,
+//optionally size and a player ID, in this case the player ID is set to 1
+//the url is an official Nes Controller compatible with the NesSmartController class
 simplePeer.createQrCode(
   "https://smartcontrollerjs.github.io/Controllers/nesController.html",
   "qrcode",
   150,
-  150
+  150,
+  "1"
 );
 
 //listen for new connections and log them in the console
@@ -22,15 +28,34 @@ simplePeer.on("connection", function (data) {
 //run an update function to continuously process the data from the phone
 processData();
 
-//a function that loops over the connected controllers and logs state of all buttons in the console if 'start' button is pressed
+//a function that checks if player 1 is connected, if yes then check the arrow keys to highlight the buttons
 function processData() {
-  for (var key in simplePeer.controllerList) {
-    //store the controller to access its fields
-    var controller = simplePeer.controllerList[key];
-    //check if start button is pressed
-    if (controller.buttons.start) {
-      //log the buttons dictionary
-      console.log(controller.buttons);
+  if (simplePeer.controllerList[1]) {
+    //store the controller to access its fields, the dictionary key is 1 because a player ID has been specified, otherwise the peer ID from smartphone will be used
+    var controller = simplePeer.controllerList[1];
+    //check if up button is pressed, if yes change the background colour from gray to yellow, then do the same for the remaining arrows
+    if (controller.buttons.up) {
+      document.getElementById("up").style.backgroundColor = "#e9ec06";
+    } else {
+      document.getElementById("up").style.backgroundColor = "#dddcdc";
+    }
+
+    if (controller.buttons.down) {
+      document.getElementById("down").style.backgroundColor = "#e9ec06";
+    } else {
+      document.getElementById("down").style.backgroundColor = "#dddcdc";
+    }
+
+    if (controller.buttons.left) {
+      document.getElementById("left").style.backgroundColor = "#e9ec06";
+    } else {
+      document.getElementById("left").style.backgroundColor = "#dddcdc";
+    }
+
+    if (controller.buttons.right) {
+      document.getElementById("right").style.backgroundColor = "#e9ec06";
+    } else {
+      document.getElementById("right").style.backgroundColor = "#dddcdc";
     }
   }
   requestAnimationFrame(processData);
