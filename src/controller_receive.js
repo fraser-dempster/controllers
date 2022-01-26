6e60dd728e6fa27d33f0961c13fabc9fa924d9f8
Emma Poliakova <2389098p@student.gla.ac.uk>
2022-01-26 11:18:51 +0000

    controller demo update

--- src/controller_receive.js
+++ src/controller_receive.js
@@ -4,7 +4,7 @@ import "smartcontroller";
 
 //create an instance of NesSmartController, optionally specify a peer ID for the PC browser peer
 //firstconnected is true by default, only the first player to connect to a specific player ID will be allowed to keep it, the rest of the connections to the same ID won't be allowed
-const simplePeer = new smartcontroller.NesSmartController("123456789");
+const simplePeer = new smartcontroller.NesSmartController();
 
 //create and display a QR code for the smartphones, specify url for the controller, div element for the code to be displayed,
 //optionally size and a player ID, in this case the player ID is set to 1
@@ -30,29 +30,14 @@ function processData() {
   if (simplePeer.controllerList[1]) {
     //store the controller to access its fields, the dictionary key is 1 because a player ID has been specified, otherwise the peer ID from smartphone will be used
     var controller = simplePeer.controllerList[1];
+    var button_id = ["up", "down", "left", "right"];
     //check if up button is pressed, if yes change the background colour from gray to yellow, then do the same for the remaining arrows
-    if (controller.buttons.up) {
-      document.getElementById("up").style.backgroundColor = "#e9ec06";
-    } else {
-      document.getElementById("up").style.backgroundColor = "#dddcdc";
-    }
-
-    if (controller.buttons.down) {
-      document.getElementById("down").style.backgroundColor = "#e9ec06";
-    } else {
-      document.getElementById("down").style.backgroundColor = "#dddcdc";
-    }
-
-    if (controller.buttons.left) {
-      document.getElementById("left").style.backgroundColor = "#e9ec06";
-    } else {
-      document.getElementById("left").style.backgroundColor = "#dddcdc";
-    }
-
-    if (controller.buttons.right) {
-      document.getElementById("right").style.backgroundColor = "#e9ec06";
-    } else {
-      document.getElementById("right").style.backgroundColor = "#dddcdc";
+    for (var id of button_id) {
+      if (controller.buttons[id]) {
+        document.getElementById(id).style.backgroundColor = "#e9ec06";
+      } else {
+        document.getElementById(id).style.backgroundColor = "#dddcdc";
+      }
     }
   }
   requestAnimationFrame(processData);
